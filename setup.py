b2de5bd844b33cf004458b3303f55313ad0d9cd6
Alexander Knieps <alexanderrobom@web.de>
2020-06-11 14:08:14 +0200

    1.1 Release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = 'fzj-ak-filenames',
-	version = '1.0',
+	version = '1.1',
 	author = 'Alexander Knieps',
 	author_email = 'a.knieps@fz-juelich.de',
 	description = 'Filename parser & writer for fusion device configuration-related files',
